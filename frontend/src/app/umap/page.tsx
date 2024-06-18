aee4f0de2cb055149d49d3b5953fd608eba33d7e
Thomas Dunlop <dunlopt12@gmail.com>
2024-06-18 12:05:27 -0400

    frontend scaffolding nearly complete

---
+++ frontend/src/app/umap/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Page() {
+
+  return (
+    <>
+      <p>Add your UMAP code here</p>
+    </>
+  );
+}
--- frontend/src/components/baseLayout.tsx
+++ frontend/src/components/baseLayout.tsx
@@ -3,7 +3,7 @@
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useRouter, usePathname } from "next/navigation";
-import { XMarkIcon } from "@heroicons/react/24/outline";
+import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/outline";
 import Navbar from "./navBar";
 
 const userNavigation = [
@@ -95,7 +95,18 @@ export default function BaseLayout({
       </div>
 
       <div className="lg:pl-72">
-        <main className="py-10 bg-white">
+      <div className="sticky top-0 z-18 flex h-16 shrink-0 items-center gap-x-4 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+          <button
+            type="button"
+            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+            onClick={() => setSidebarOpen(true)}
+          >
+            <span className="sr-only">Open sidebar</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+
+        <main className="py-2 bg-white">
           <div className="px-4 sm:px-6 lg:px-8">{children}</div>
         </main>
       </div>
