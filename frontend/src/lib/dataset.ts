66dbea84491ace3a28d730cad02e2868bca3e1ef
Thomas Dunlop <dunlopt12@gmail.com>
2024-06-18 13:56:17 -0400

    adding dataset typing

--- frontend/src/lib/dataset.ts
+++ frontend/src/lib/dataset.ts
@@ -1,8 +1,13 @@
 import useSWR from 'swr'
 import { fetchJson } from './fethcing'
 
+type Dataset = {
+    id: number,
+    name: string
+}
+
 export function useDatasets () {
-    const { data, error, isLoading } = useSWR('/api/dataset/', fetchJson)
+    const { data, error, isLoading } = useSWR<Dataset[]>('/api/dataset/', fetchJson)
    
     return {
       datasets: data,
