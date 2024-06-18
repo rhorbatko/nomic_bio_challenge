4d8618f21030418dd596725eacea11f446ba5b56
Thomas Dunlop <dunlopt12@gmail.com>
2024-06-18 10:16:56 -0400

    adding models

--- graphing_app/api/models.py
+++ graphing_app/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+import orjson
 
-# Create your models here.
+
+class ORJSONDecodedField(models.JSONField):
+
+    def from_db_value(self, value, expression, connection):
+        if value is None:
+            return value
+        try:
+            return orjson.loads(value)
+        except orjson.JSONDecodeError:
+            return value
+
+
+class Target(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+
+class Dataset(models.Model):
+    name = models.CharField(max_length=120)
+
+
+class Sample(models.Model):
+    dataset = models.ForeignKey("Dataset", on_delete=models.CASCADE)
+    metadata = ORJSONDecodedField(null=True)
+
+
+class SampleSignal(models.Model):
+    target = models.ForeignKey("Target", on_delete=models.CASCADE)
+    sample = models.ForeignKey("Sample", on_delete=models.CASCADE)
+    signal = models.FloatField()
+
+
+class UmapPlotPoint(models.Model):
+    x_coor = models.FloatField(null=False)
+    y_coor = models.FloatField(null=False)
+    sample = models.ForeignKey("Sample", on_delete=models.CASCADE)
